44916186ac09f90e0a97a919d323f963fc3c61e1
Peter Fortune <pfortune@gmail.com>
2024-06-14 02:11:17 +0100

    Add to favourites logic completed; Dummy favourites page.

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -19,7 +19,7 @@ const styles = {
   },
 };
 
-  const MovieListPage: React.FC= () => {
+  const MovieListPage: React.FC = () => {
     const [movies, setMovies] = useState<BaseMovieProps[]>([]);
     const [titleFilter, setTitleFilter] = useState("");
     const [genreFilter, setGenreFilter] = useState("0");
@@ -38,6 +38,11 @@ const styles = {
     const handleChange = (type: FilterOption, value: string) => {
       if (type === "title") setTitleFilter(value);
       else setGenreFilter(value);
+    };
+
+    const addToFavourites = (movieId: number) => {
+      const updatedMovies = movies.map((m: BaseMovieProps) => m.id === movieId ? { ...m, favourite: true } : m);
+      setMovies(updatedMovies);
     };
 
     useEffect(() => {
@@ -62,7 +67,7 @@ const styles = {
           <Header title={"Home Page"} />
         </Grid>
         <Grid item container spacing={5}>
-          <MovieList movies={displayedMovies}></MovieList>
+          <MovieList movies={displayedMovies} selectFavourite={addToFavourites}></MovieList>
         </Grid>
       </Grid>
       <Fab
