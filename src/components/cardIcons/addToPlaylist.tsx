a56c28d91ab09520a922da03e825bbf9ee3337d0
Peter Fortune <pfortune@gmail.com>
2024-06-23 23:51:31 +0100

    Tagging upcoming movies as must-watch

--- src/components/cardIcons/addToPlaylist.tsx
+++ src/components/cardIcons/addToPlaylist.tsx
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { MouseEvent, useContext}  from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
-import {BaseMovieProps} from "../../types/interfaces"
+import { BaseMovieProps } from "../../types/interfaces"
 
-const AddToPlaylistIcon: React.FC<BaseMovieProps> = () => {
+const AddToPlaylistIcon: React.FC<BaseMovieProps> = (movie) => {
+  const context = useContext(MoviesContext);
 
+  const onUserSelect = (e: MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    context.addToPlaylist(movie);
+  };
   return (
-    <IconButton aria-label="add to playlist">
+    <IconButton aria-label="add to playlist" onClick={onUserSelect}>
       <PlaylistAddIcon color="primary" fontSize="large" />
     </IconButton>
   );
--- src/contexts/moviesContext.tsx
+++ src/contexts/moviesContext.tsx
@@ -1,28 +1,37 @@
 import React, { useState, useCallback } from "react";
 import { BaseMovieProps, Review } from "../types/interfaces";
 
-
 interface MovieContextInterface {
     favourites: number[];
-    addToFavourites: ((movie: BaseMovieProps) => void);
-    removeFromFavourites: ((movie: BaseMovieProps) => void);
-    addReview: ((movie: BaseMovieProps, review: Review) => void);
+    addToFavourites: (movie: BaseMovieProps) => void;
+    removeFromFavourites: (movie: BaseMovieProps) => void;
+    addReview: (movie: BaseMovieProps, review: Review) => void;
+    reviews: { [key: number]: Review };
+    playlist: number[];
+    addToPlaylist: (movie: BaseMovieProps) => void;
+    removeFromPlaylist: (movie: BaseMovieProps) => void;
 }
+
 const initialContextState: MovieContextInterface = {
     favourites: [],
+    reviews: {},
+    playlist: [],
     addToFavourites: () => {},
     removeFromFavourites: () => {},
-    addReview: (movie, review) => { movie.id, review },
+    addReview: () => {},
+    addToPlaylist: () => {},
+    removeFromPlaylist: () => {},
 };
 
 export const MoviesContext = React.createContext<MovieContextInterface>(initialContextState);
 
 const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
     const [favourites, setFavourites] = useState<number[]>([]);
-    const [myReviews, setMyReviews] = useState<Review[]>([]);
+    const [reviews, setReviews] = useState<{ [key: number]: Review }>({});
+    const [playlist, setPlaylist] = useState<number[]>([]);
 
     const addReview = (movie: BaseMovieProps, review: Review) => {
-        setMyReviews( { ...myReviews, [movie.id]: review })
+        setReviews((prevReviews) => ({ ...prevReviews, [movie.id]: review }));
     };
 
     const addToFavourites = useCallback((movie: BaseMovieProps) => {
@@ -38,13 +47,34 @@ const MoviesContextProvider: React.FC<React.PropsWithChildren> = ({ children }) 
         setFavourites((prevFavourites) => prevFavourites.filter((mId) => mId !== movie.id));
     }, []);
 
+    const addToPlaylist = useCallback((movie: BaseMovieProps) => {
+        setPlaylist((prevPlaylist) => {
+            if (!prevPlaylist.includes(movie.id)) {
+                console.log("Adding movie to playlist:", movie); 
+                return [...prevPlaylist, movie.id];
+            }
+            return prevPlaylist;
+        });
+    }, []);
+
+    const removeFromPlaylist = useCallback((movie: BaseMovieProps) => {
+        setPlaylist((prevPlaylist) => {
+            console.log("Removing movie from playlist:", movie); 
+            return prevPlaylist.filter((mId) => mId !== movie.id);
+        });
+    }, []);
+
     return (
         <MoviesContext.Provider
             value={{
                 favourites,
+                reviews,
+                playlist,
                 addToFavourites,
                 removeFromFavourites,
                 addReview,
+                addToPlaylist,
+                removeFromPlaylist,
             }}
         >
             {children}
